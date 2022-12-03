d5abb24aa221ac7164258484b895dff69dff8c2a
Bulygin Aleksey <alex987@bk.ru>
2022-12-04 01:23:38 +0300

    Http service, logger service, toastify

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+import httpService from "../services/http.service";
 
 const EditQualityPage = () => {
-    return (
-        <>
-            <h1>Edit Quality Page</h1> <EditForm />
-        </>
-    );
+  const [quality, setQuality] = useState(null);
+  const id = useParams().id;
+
+  const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await httpService.get(qualityEndPoint);
+        setQuality(data.content);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, [id]);
+
+  const handleSubmit = async (data) => {
+    try {
+      await httpService
+        .put(qualityEndPoint, data)
+        .then((res) => console.log(res.data.content));
+    } catch (error) {
+      console.log("Expected error");
+    }
+  };
+
+  return (
+    <>
+      <h1>Edit Quality Page</h1>{" "}
+      {quality !== null ? (
+        <EditForm
+          data={quality}
+          onSubmit={handleSubmit}
+        />
+      ) : (
+        "Loading ..."
+      )}
+    </>
+  );
 };
 
 export default EditQualityPage;
---
+++ src/app/services/log.service.js
@@ -0,0 +1,24 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://f8ac74c849db4698ae1b9960bd33e109@o4504266689282048.ingest.sentry.io/4504266998480896",
+    integrations: [new BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+const logger = {
+  init,
+  log,
+};
+
+export default logger;
